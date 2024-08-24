3c7a0838f425e5ddb8721585ad6bbac411ff26ac
rekkedhammer <hamirhamirhamir456@gmail.com>
2024-08-24 20:28:22 +0800

    change some settings

--- google_oauth2/settings.py
+++ google_oauth2/settings.py
@@ -34,10 +34,6 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000", "https://web-production-ee30.up.railway.app"]
 
-
-
-# Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
